910a03051ff7473d0a1b10c72a9d8894e420abf3
Johan Cwiklinski <jcwiklinski@teclib.com>
2020-12-03 18:02:18 +0100

    Rely on curl, fail on empty contents

--- lib/php/FilesToJSON.php
+++ lib/php/FilesToJSON.php
@@ -136,9 +136,10 @@ class FilesToJSON
 
         $interval = strtotime('-1 week');
         if (!file_exists($path) || filemtime($path) <= $interval) {
+            $contents = $this->callCurl($uri);
             file_put_contents(
                 $path,
-                file_get_contents($uri)
+                $contents
             );
         }
         return fopen($path, 'r');
@@ -245,5 +246,61 @@ class FilesToJSON
         }
 
         return file_put_contents($this->getPathFor('iftype'), json_encode($iftypes, JSON_PRETTY_PRINT));
+    }
+
+    /**
+     * Executes a curl call
+     *
+     * @param string $url        URL to retrieve
+     * @param array  $eopts      Extra curl opts
+     * @param string $msgerr     will contains a human readable error string if an error occurs of url returns empty contents
+     * @param string $curl_error will contains original curl error string if an error occurs
+     *
+     * @return string
+     */
+    public function callCurl($url, array $eopts = [], &$msgerr = null, &$curl_error = null)
+    {
+        $content = '';
+        $taburl  = parse_url($url);
+
+        $defaultport = 80;
+
+        // Manage standard HTTPS port : scheme detection or port 443
+        if ((isset($taburl["scheme"]) && $taburl["scheme"]=='https')
+            || (isset($taburl["port"]) && $taburl["port"]=='443')) {
+            $defaultport = 443;
+        }
+
+        $ch = curl_init($url);
+        $opts = [
+            CURLOPT_URL             => $url,
+            CURLOPT_USERAGENT       => "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)",
+            CURLOPT_RETURNTRANSFER  => 1,
+        ] + $eopts;
+
+        curl_setopt_array($ch, $opts);
+        $content = curl_exec($ch);
+        $curl_error = curl_error($ch) ?: null;
+        curl_close($ch);
+
+        if ($curl_error !== null) {
+            if (empty($CFG_GLPI["proxy_name"])) {
+                //TRANS: %s is the error string
+                $msgerr = sprintf(
+                    'Connection failed. (%s)',
+                    $curl_error
+                );
+            }
+            $content = '';
+        } elseif (empty($content)) {
+            $msgerr = sprintf(
+                'No data available on %s',
+                $url
+            );
+        }
+        if (!empty($msgerr)) {
+            throw new \RuntimeException($msgerr);
+        }
+        return $content;
     }
 }
