b3858d7c2534f4afa781f3f59442ef78b02fee80
Johan Cwiklinski <johan@x-tnd.be>
2022-01-21 08:40:35 +0100

    Be strict testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * ---------------------------------------------------------------------
  * GLPI - Gestionnaire Libre de Parc Informatique
@@ -44,3 +45,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../lib/php/Converter.php';
 
 define('TU_DIR', __DIR__);
+define('TU_USER', '_test_user');
