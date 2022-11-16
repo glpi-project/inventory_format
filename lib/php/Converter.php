2b8bc73f50acf7330700f9c2fc7add6c31ba2d5e
Vadim Pisarev <plv@techiemedia.net>
2022-11-16 08:52:39 -0500

    bugfix: netport['aggregate']['port'] may be a string (#83)
    * fix: ['aggregate']['port'] may be a string
    
    * Update lib/php/Converter.php
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    
    * reverted 2704156
    
    * lint fix
    
    * Update lib/php/Converter.php
    
    * Update lib/php/Converter.php
    
    * Add test file
    
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>
    Co-authored-by: Johan Cwiklinski <johan@x-tnd.be>

--- lib/php/Converter.php
+++ lib/php/Converter.php
@@ -1438,9 +1438,10 @@ class Converter
                                 [$netport['connections']['connection']];
                         }
                         if (isset($netport['aggregate'])) {
-                            $netport['aggregate'] = array_is_list($netport['aggregate']['port']) ?
-                                $netport['aggregate']['port'] :
-                                [$netport['aggregate']['port']];
+                            $netport['aggregate'] = is_array($netport['aggregate']['port'])
+                                && array_is_list($netport['aggregate']['port'])
+                                ? $netport['aggregate']['port']
+                                : [$netport['aggregate']['port']];
                             $netport['aggregate'] = array_map('intval', $netport['aggregate']);
                         }
 
