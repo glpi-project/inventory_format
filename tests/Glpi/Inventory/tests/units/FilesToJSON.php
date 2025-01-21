2517218e58b64ceea4b6e5b24d14604015e48e58
Johan Cwiklinski <johan@x-tnd.be>
2025-01-21 08:39:32 +0100

    Fix license headers, use GLPI phpcs configuration

--- tests/Glpi/Inventory/tests/units/FilesToJSON.php
+++ tests/Glpi/Inventory/tests/units/FilesToJSON.php
@@ -1,41 +1,12 @@
 <?php
+
 /**
- * ---------------------------------------------------------------------
- * GLPI - Gestionnaire Libre de Parc Informatique
- * Copyright (C) 2015-2018 Teclib' and contributors.
+ * © Teclib' and contributors.
  *
- * http://glpi-project.org
+ * This file is part of GLPI inventory format.
  *
- * based on GLPI - Gestionnaire Libre de Parc Informatique
- * Copyright (C) 2003-2014 by the INDEPNET Development Team.
- *
- * ---------------------------------------------------------------------
- *
- * LICENSE
- *
- * This file is part of GLPI.
- *
- * GLPI is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * GLPI is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
- * ---------------------------------------------------------------------
- *
- * PHP version 7
- *
- * @category  Inventory
- * @package   Glpi
- * @author    Johan Cwiklinski <jcwiklinski@teclib.com>
- * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
- * @link      https://glpi-project.org
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Glpi\Inventory\tests\units;
@@ -45,12 +16,7 @@ use PHPUnit\Framework\TestCase;
 /**
  * Tests for hardware file conversions
  *
- * @category  Inventory
- * @package   GlpiTests
  * @author    Johan Cwiklinski <jcwiklinski@teclib.com>
- * @copyright 2019 GLPI Team and Contributors
- * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
- * @link      https://glpi-project.org
  */
 class FilesToJSON extends TestCase
 {
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,44 +1,12 @@
 <?php
 
 /**
- * ---------------------------------------------------------------------
- * GLPI - Gestionnaire Libre de Parc Informatique
- * Copyright (C) 2015-2018 Teclib' and contributors.
+ * © Teclib' and contributors.
  *
- * http://glpi-project.org
+ * This file is part of GLPI inventory format.
  *
- * based on GLPI - Gestionnaire Libre de Parc Informatique
- * Copyright (C) 2003-2014 by the INDEPNET Development Team.
- *
- * ---------------------------------------------------------------------
- *
- * LICENSE
- *
- * This file is part of GLPI.
- *
- * GLPI is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * GLPI is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with GLPI. If not, see <http://www.gnu.org/licenses/>.
- * ---------------------------------------------------------------------
- *
- * PHP version 7
- *
- * @category  Tests
- * @package   Glpi
- *
- * @author    Johan Cwiklinski <johan@x-tnd.be>
- * @copyright 2018 GLPI Team and Contributors
- * @license   http://www.gnu.org/licenses/gpl-3.0.html GPL License 3.0 or (at your option) any later version
- * @link      https://glpi-project.org
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 require_once __DIR__ . '/../vendor/autoload.php';
